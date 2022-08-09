7c68d97127397431721914213d1d788f0e6ab3b7
Greg Sanders <gsanders87@gmail.com>
2022-08-09 10:27:07 -0400

    Implement hsm eltoo channel ready msg

--- hsmd/hsmd.c
+++ hsmd/hsmd.c
@@ -672,12 +672,6 @@ static struct io_plan *handle_client(struct io_conn *conn, struct client *c)
     case WIRE_HSMD_COMBINE_PSIG:
     case WIRE_HSMD_VALIDATE_UPDATE_TX_PSIG:
     case WIRE_HSMD_GET_NONCE:
-
-    case WIRE_HSMD_READY_ELTOO_CHANNEL_REPLY:
-    case WIRE_HSMD_PSIGN_UPDATE_TX_REPLY:
-    case WIRE_HSMD_COMBINE_PSIG_REPLY:
-    case WIRE_HSMD_VALIDATE_UPDATE_TX_PSIG_REPLY:
-    case WIRE_HSMD_GET_NONCE_REPLY:
     /* Eltoo stuff ends */
 		/* Hand off to libhsmd for processing */
 		return req_reply(conn, c,
@@ -707,6 +701,11 @@ static struct io_plan *handle_client(struct io_conn *conn, struct client *c)
 	case WIRE_HSMD_SIGN_MESSAGE_REPLY:
 	case WIRE_HSMD_GET_OUTPUT_SCRIPTPUBKEY_REPLY:
 	case WIRE_HSMD_SIGN_BOLT12_REPLY:
+    case WIRE_HSMD_READY_ELTOO_CHANNEL_REPLY:
+    case WIRE_HSMD_PSIGN_UPDATE_TX_REPLY:
+    case WIRE_HSMD_COMBINE_PSIG_REPLY:
+    case WIRE_HSMD_VALIDATE_UPDATE_TX_PSIG_REPLY:
+    case WIRE_HSMD_GET_NONCE_REPLY:
 		return bad_req_fmt(conn, c, c->msg_in,
 				   "Received an incoming message of type %s, "
 				   "which is not a request",
