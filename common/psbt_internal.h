885ffb5ccbdd59f74b163e62dfc5343cdc3c9451
Dustin Dettmer <dustinpaystaxes@gmail.com>
2023-03-05 16:55:58 -0500

    psbt: routine for lining up sigantures in a witness stack
    Changelog-None

--- common/psbt_internal.h
+++ common/psbt_internal.h
@@ -20,6 +20,24 @@ struct witness_element;
 void psbt_finalize_input(const tal_t *ctx,
 			 struct wally_psbt_input *in,
 			 const struct witness_element **elements);
+/* psbt_finalize_multisig_signatures - Takes signatures from the
+ * "partial signatures" section and adds them to the witness stack
+ * in the order their corresponding public keys appear in the witness
+ * script. If the signature is already present, it is not added.
+ *
+ * If the stack is empty, the witness_script is appended and a zero
+ * size signature is prepended.
+ *
+ * The signatures are DER encoded if not already.
+ *
+ * @ctx - the context to allocate onto
+ * @in - input to set final_witness for
+ *
+ * Returns the number of signatures that were recognized as correct
+ * for the witness script (whether they were already present or not).
+ */
+int psbt_finalize_multisig_signatures(const tal_t *ctx,
+				       struct wally_psbt_input *in);
 /* psbt_to_witness_stacks - Take all sigs on a PSBT and copy to a
  * 			    witness_stack
  *
@@ -33,5 +51,13 @@ psbt_to_witness_stacks(const tal_t *ctx,
 		       const struct wally_psbt *psbt,
 		       enum tx_role side_to_stack,
                        int input_index_to_ignore);
+
+/* psbt_input_weight - Calculate the tx weight for input index `in` */
+size_t psbt_input_weight(struct wally_psbt *psbt,
+                                size_t in);
+
+/* psbt_output_weight - Calculate the tx weight for output index `outnum` */
+size_t psbt_output_weight(struct wally_psbt *psbt,
+                          size_t outnum);
 
 #endif /* LIGHTNING_COMMON_PSBT_INTERNAL_H */
